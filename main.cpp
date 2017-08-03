edb6c411c7b3b10e70c2edd3b0f70be136c87ba4
vikchopde <chopde.vikram@gmail.com>
2017-08-04 01:15:42 +0800

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 
 int main()
 {
-    typedef std::integral_constant<int, 3> two_t;
-    typedef std::integral_constant<int, 4> four_t;
+    typedef metavik::integral_constant<int, 3> two_t;
+    typedef metavik::integral_constant<int, 4> four_t;
     static_assert(two_t::value * 2 == four_t::value, "something broke");
     return 0;
 }
